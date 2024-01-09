493b62054540f9adbd72495e6c70902bb873b0a1
linoreki <148707553+linoreki@users.noreply.github.com>
2024-01-09 11:35:20 +0100

    Update IPtaker.py

--- IPtaker.py
+++ IPtaker.py
@@ -1,4 +1,7 @@
 import requests
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 def get_public_ip():
     """
@@ -24,6 +27,33 @@ def save_ip_to_file(ip):
     except Exception as e:
         print(f"Error al guardar la IP pública: {e}")
 
+def send_email(subject, body):
+    """
+    Envía un correo electrónico con la dirección IP pública.
+    """
+    try:
+        sender_email = 'tucorreo@gmail.com'  # Tu dirección de correo electrónico
+        receiver_email = 'destinatario@gmail.com'  # La dirección de correo electrónico del destinatario
+        password = 'tucontraseña'  # Tu contraseña de correo electrónico
+
+        # Configura el mensaje
+        message = MIMEMultipart()
+        message['From'] = sender_email
+        message['To'] = receiver_email
+        message['Subject'] = subject
+        message.attach(MIMEText(body, 'plain'))
+
+        # Conéctate al servidor SMTP y envía el correo electrónico
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.starttls()
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+            
+        print("Correo electrónico enviado correctamente.")
+        
+    except Exception as e:
+        print(f"Error al enviar el correo electrónico: {e}")
+
 def main():
     # Obtiene la dirección IP pública
     public_ip = get_public_ip()
@@ -31,6 +61,11 @@ def main():
     if public_ip:
         # Guarda la dirección IP pública en un archivo de texto
         save_ip_to_file(public_ip)
+
+        # Envía la dirección IP pública por correo electrónico
+        subject = 'Dirección IP Pública'
+        body = f"La dirección IP pública actual es: {public_ip}"
+        send_email(subject, body)
     else:
         print("No se pudo obtener la IP pública.")
 
